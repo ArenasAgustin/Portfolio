2c29751f576ce905f8d55983aec48e43fb168795
“ArenasAgustin” <arenasagustin7@gmail.com>
2021-11-14 17:38:36 -0300

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import Cv from '../../assets/resume.pdf';
+import Profile from '../../assets/profile.jpg';
 import './About.css';
 
 export default function About() {
     return (
         <div className="about" id='about'>
+            <h1 className='about-title'>Sobre mi</h1>
+
             <div className='about-container'>
+                <div className='about-image'>
+                    <img src={Profile} alt='profile' />
+                </div>
 
-            </div>
-            <div className="about-card">
-                <h1>Sobre mi</h1>
+                <div className="about-card">
+                    <p className="about-txt">
+                        Programador y web developer, imaginativo, curioso y capaz de resolver diversos problemas. Soy un programador dispuesto a ayudar a mis colegas.
+                    </p>
+                    <p className="about-txt">
+                        Tengo interés por el estudio de diferentes tecnologías. En la actualidad estoy aprendiendo por mi cuenta el uso de Typescript y React Native. También disfruto con la música y el dibujo.
+                    </p>
 
-                <p className="about-txt">
-                    Programador y web developer, imaginativo, curioso y capaz de resolver diversos problemas. Soy un programador dispuesto a ayudar a mis colegas.
-                </p>
-                <p className="about-txt">
-                    Tengo interés por el estudio de diferentes tecnologías. En la actualidad estoy aprendiendo por mi cuenta el uso de Typescript y React Native. También disfruto con la música y el dibujo.
-                </p>
-                <a download href={Cv} className="about-btn">Ver resumen</a>
+                    <a download href={Cv} className="about-btn-a">
+                        <p className="btn-about">Ver resumen</p>
+                    </a>
+                </div>
             </div>
         </div>
     )
