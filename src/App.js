13aca9dc4b75a1dc9b6686e1762752b4e9f190c8
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-02-22 14:40:18 -0300

    arreglos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-      </Routes>
+      <Home />
     </div>
   );
 }
