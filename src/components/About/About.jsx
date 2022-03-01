1f8794e13541c85631a91cca62edfac21a10e6c9
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-03-01 19:53:17 -0300

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -30,9 +30,149 @@ export default function About() {
         </div>
       </div>
 
-      <div>
-        <div>
-          <img />
+      <div className="about-container">
+        <div className="about-technology">
+          <span className="about-technology__txt">JavaScript</span>
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+            className="about-technology__img"
+          />
+        </div>
+
+        <div className="about-technology">
+          <span className="about-technology__txt">TypeScript</span>
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+            className="about-technology__img"
+          />
+        </div>
+
+        <div className="about-technology">
+          <span className="about-technology__txt">React</span>
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+            className="about-technology__img"
+          />
+        </div>
+
+        <div className="about-technology">
+          <span className="about-technology__txt">Redux</span>
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg"
+            className="about-technology__img"
+          />
+        </div>
+
+        <div className="about-technology">
+          <span className="about-technology__txt">Node.js</span>
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+            className="about-technology__img"
+          />
+        </div>
+
+        <div className="about-technology">
+          <span className="about-technology__txt">Css</span>
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+            className="about-technology__img"
+          />
+        </div>
+
+        <div className="about-technology">
+          <span className="about-technology__txt">Sass</span>
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg"
+            className="about-technology__img"
+          />
+        </div>
+
+        <div className="about-technology">
+          <span className="about-technology__txt">Less</span>
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/less/less-plain-wordmark.svg"
+            className="about-technology__img"
+          />
+        </div>
+
+        <div className="about-technology">
+          <span className="about-technology__txt">Html</span>
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
+            className="about-technology__img"
+          />
+        </div>
+
+        <div className="about-technology">
+          <span className="about-technology__txt">PostgresSQL</span>
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg"
+            className="about-technology__img"
+          />
+        </div>
+
+        <div className="about-technology">
+          <span className="about-technology__txt">Sequelize</span>
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg"
+            className="about-technology__img"
+          />
+        </div>
+
+        <div className="about-technology">
+          <span className="about-technology__txt">MongoDB</span>
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
+            className="about-technology__img"
+          />
+        </div>
+
+        <div className="about-technology">
+          <span className="about-technology__txt">Mongoose</span>
+
+          <img
+            src="https://mongoosejs.com/docs/images/mongoose5_62x30_transparent.png"
+            className="about-technology__img mongoose"
+          />
+        </div>
+
+        <div className="about-technology">
+          <span className="about-technology__txt">Express</span>
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
+            className="about-technology__img"
+          />
+        </div>
+
+        <div className="about-technology">
+          <span className="about-technology__txt">JSON</span>
+
+          <img
+            src="https://cdn.worldvectorlogo.com/logos/json.svg"
+            className="about-technology__img"
+          />
+        </div>
+
+        <div className="about-technology">
+          <span className="about-technology__txt">Git</span>
+
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+            className="about-technology__img"
+          />
         </div>
       </div>
     </div>
